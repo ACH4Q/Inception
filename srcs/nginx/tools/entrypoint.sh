027c4c891bd59f0d573d1ef58838066750af47c8
ACH4Q <achaqmohammedamine1@gmail.com>
2025-09-30 05:08:54 +0100

    Correcting Wordpress conf pm and Dockerfile disabling sudo

--- srcs/nginx/tools/entrypoint.sh
+++ srcs/nginx/tools/entrypoint.sh
@@ -3,9 +3,9 @@
 SSL_CERT="/etc/nginx/ssl/inception.crt"
 SSL_KEY="/etc/nginx/ssl/inception.key"
 
-if [! -f "${SSL_CERT}"]; then
-    mkdir -p /etc/ngnix/ssl
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "{$SSL_KEY}" -out "{$SSL_CERT}" -subj "/C=MA/ST=BENGREUIR/L=Bengreuir/O=1337/OU=student/CN=${DOMAIN_NAME}"
+if [ ! -f "${SSL_CERT}" ]; then
+    mkdir -p /etc/nginx/ssl
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "${SSL_KEY}" -out "${SSL_CERT}" -subj "/C=MA/ST=BENGUERIR/L=Benguerir/O=1337/OU=student/CN=${DOMAIN_NAME}"
 fi
 
 exec "$@"
