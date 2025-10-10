f5e7d46f71f6b4961f98c685f195698f58ec57c8
ACH4Q <achaqmohammedamine1@gmail.com>
2025-10-10 13:48:32 +0100

    adding redis cache downloding on wordpress

--- srcs/wordpress/tools/setup.sh
+++ srcs/wordpress/tools/setup.sh
@@ -15,6 +15,11 @@ if [ ! -f "wp-config.php" ]; then
     wp config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --allow-root
     wp core install --url=${DOMAIN_NAME}  --title="Inception" --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --allow-root
     wp user create ${WP_USER} ${WP_USER_EMAIL}  --role=author  --user_pass=${WP_USER_PASSWORD}  --allow-root
+
+    echo "Enabling Redis cache..."
+    wp plugin install redis-cache --activate --allow-root
+    wp redis enable --allow-root
+    
     echo "WordPress installation complete."
 else
     echo "WordPress is already installed."
